6abd27f2f455d7a6a1220eae648f1ac5c37305d1
Advisorrry <sypikeh@yandex.com>
2021-11-26 17:48:12 +0500

    bug: пока не получается мигрировать с v4 до v5 mui

--- theme.ts
+++ theme.ts
@@ -1,21 +1,24 @@
-import { createTheme } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
-  overrides: {
+  components: {
     MuiButton: {
-      root: {
-        borderRadius: '10px',
-      },
-      contained: {
-        backgroundColor: 'white',
-        boxShadow:
-          '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)',
-        '&:hover': {
+      styleOverrides: {
+        root: {
+          borderRadius: '10px',
+        },
+        contained: {
           backgroundColor: 'white',
           boxShadow:
-            '0 1px 1px rgb(0 0 0 / 18%), 0 4px 7px rgb(0 0 0 / 8%), 0 -1px 0 rgb(0 0 0 / 8%), -1px 0 0 rgb(0 0 0 / 8%), 1px 0 0 rgb(0 0 0 / 15%)',
+            '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)',
+          '&:hover': {
+            backgroundColor: 'white',
+            boxShadow:
+              '0 1px 1px rgb(0 0 0 / 18%), 0 4px 7px rgb(0 0 0 / 8%), 0 -1px 0 rgb(0 0 0 / 8%), -1px 0 0 rgb(0 0 0 / 8%), 1px 0 0 rgb(0 0 0 / 15%)',
+          },
         },
       },
     },
   },
 })
+

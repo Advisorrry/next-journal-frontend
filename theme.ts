9480d441a0095d4bede10f7f82db887825cd7233
Advisorrry <sypikeh@yandex.com>
2021-11-27 13:42:25 +0500

    наконец-то мигрировал с mui v4 на v5... Это было сложно(рил)

--- theme.ts
+++ theme.ts
@@ -2,23 +2,65 @@ import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
   components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        rounded: {
+          borderRadius: 8,
+        },
+      },
+    },
+
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          boxShadow: 'none',
+        },
+      },
+    },
+
     MuiButton: {
       styleOverrides: {
         root: {
-          borderRadius: '10px',
+          borderRadius: '8px',
+          color: '#000',
+          textTransform: 'inherit',
+          fontSize: 16,
+          transition: 'none',
+          '&:active': {
+            boxShadow:
+              '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 0%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%) !important',
+            transform: 'translateY(1px)',
+          },
         },
         contained: {
-          backgroundColor: 'white',
+          backgroundColor: '#white',
+          color: '#fff',
           boxShadow:
             '0 1px 1px rgb(0 0 0 / 15%), 0 4px 7px rgb(0 0 0 / 5%), 0 -1px 0 rgb(0 0 0 / 5%), -1px 0 0 rgb(0 0 0 / 5%), 1px 0 0 rgb(0 0 0 / 5%)',
           '&:hover': {
-            backgroundColor: 'white',
+            backgroundColor: '#white',
             boxShadow:
               '0 1px 1px rgb(0 0 0 / 18%), 0 4px 7px rgb(0 0 0 / 8%), 0 -1px 0 rgb(0 0 0 / 8%), -1px 0 0 rgb(0 0 0 / 8%), 1px 0 0 rgb(0 0 0 / 15%)',
+          },
+        },
+
+        containedPrimary: {
+          backgroundColor: '#4683d9',
+          '&:hover': {
+            backgroundColor: '#437CCE',
           },
         },
       },
     },
   },
+  palette: {
+    primary: {
+      main: '#4683d9',
+    },
+  },
 })
-

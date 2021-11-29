2d8b20c91f7e43b96e8cd82dcf3fa1f30d1d00d7
Advisorrry <sypikeh@yandex.com>
2021-11-29 14:00:14 +0500

    новые страницы

---
+++ pages/follows.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainLayout } from '../layouts/MainLayout'
+
+const Follows = () => {
+  return (
+    <MainLayout className="main-layout-white">
+      <div>Подписки</div>
+    </MainLayout>
+  )
+}
+
+export default Follows
---
+++ pages/messages.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MainLayout } from '../layouts/MainLayout'
+
+const Messages = () => {
+  return (
+    <MainLayout className="main-layout-white">
+      <div>Сообщения</div>
+    </MainLayout>
+  )
+}
+
+export default Messages
